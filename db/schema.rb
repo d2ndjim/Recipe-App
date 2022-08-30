543df1ca524bd4d384c7efe4f13d38938456e29d
Lekan <jimoridwanlekan@gmail.com>
2022-08-30 14:33:34 +0100

    Add database yml configuration

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_211402) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "role", default: "default", null: false
-    t.string "name", default: "default", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
